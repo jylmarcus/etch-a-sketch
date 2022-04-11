fb98402f839bec406826d4ed908e8c2a89e3042a
ljymarcus <maplecow@gmail.com>
2022-04-11 15:21:12 +0800

    Added feature to change grid size up to 100x100

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+function reset(){
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach(element => element.style.background = "white");
+}
+
+function selectDim(){
+    let input = document.querySelector("#dimensions");
+    createCells(input.value);
+    input.value = "";
+}
+
 function createCells(n) {
+    n = Number(n);
+    if(n > 100 || typeof(n) != "number" || n == NaN || n <= 0) {
+        alert("Please enter a positive number less than or equal 100.");
+        return;
+    }
     let divQuantity = n*n;
     for (i = 0; i < divQuantity; i++){
         let cell = document.createElement("div");
         cell.className = "cell";
         cell.style.background = "white";
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = "black";
+        })
         grid.appendChild(cell);
     }
     grid.style["grid-template-columns"] = `repeat(${n}, 1fr)`;
     grid.style["grid-template-rows"] = `repeat(${n},1fr)`;
 }
+
 
 let grid = document.querySelector("#grid");
 grid.style.display = "grid";
@@ -18,17 +38,7 @@ grid.style.gap = "1px";
 grid.style.background = "black";
 grid.style.border = "1px solid black";
 
-let reset = document.querySelector("#reset");
-reset.addEventListener('click', function(){
-    cells.forEach(element => element.style.background = "white");
-})
 
-createCells(16);
-
-let cells = document.querySelectorAll(".cell");
-cells.forEach(element => element.addEventListener('mouseover', function(){
-    element.style.background = "black";
-}))
 
 
 
